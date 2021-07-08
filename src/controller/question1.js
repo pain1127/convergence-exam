51859b8402ab704773812388e0902790417be346
pain1127 <sk.park@catenoid.net>
2021-07-08 11:31:43 +0900

    문제 description 수정

--- src/controller/question1.js
+++ src/controller/question1.js
@@ -3,7 +3,7 @@ const router = new express.Router();
 const users = require('../data/user');
 
 /**
- * question1 : 전체 user중 user의 나이가 30살 미만을 찾고
+ * question1 : 전체 user중 user의 나이가 30살 초과 and 소속이 카테노이드인 사용자를 찾는다.
  * 조건에 포함되는 user의 id를 문자 역순으로 nickname이라는 속성을 추가 하여 response한다.
  * @function
  * @name get/
